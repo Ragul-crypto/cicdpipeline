f2e023a9748fcc319424257f14d2f76e8a31ec9f
Ragul-crypto <awsisworld@gmail.com>
2025-04-16 11:50:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from inside the Docker container World!"
+    return "Hello from Flask app in ECS!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=3000)
+    app.run(host='0.0.0.0', port=80)
