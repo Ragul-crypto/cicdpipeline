b34365814ab7c13ec52bcb728f77988e2a8ee4aa
Ragul-crypto <awsisworld@gmail.com>
2025-04-22 14:52:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Catch root path
-@app.route("/")
-def index():
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def catch_all(path):
     return "Hello from the Ragul to the World!"
 
-# Catch all other paths
-@app.route("/<path:dummy>")
-def catch_all(dummy):
-    return f"Hello from the Ragul to the World! (You hit /{dummy})"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
